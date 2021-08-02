775ba1ac7d583eae6bf96c4eb394a5c297d22006
Andrew <wing_andrew@hotmail.com>
2021-08-01 21:24:12 -0400

    Reworking handlebars

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-
+const exhbs = require('express-handlebars');
+const hbs = exhbs.create({});
+const path = require('path');
+const dotenv = require('dotenv').config()
 const sequelize = require('./config/connection');
+const { url } = require('inspector');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -12,13 +16,31 @@ const sess = {
   secret: process.env.SECRET,
   cookie: {},
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: new SequelizeStore({
     db: sequelize
   })
 };
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//post request 
+
+app.post('/api'), (request,response)=> { 
+  console.log(request);
+}
+app.use(session({
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
