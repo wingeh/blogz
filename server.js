3204ba10211b0b1b2aa6a27062240a09abd890f6
Andrew <wing_andrew@hotmail.com>
2021-08-01 21:39:04 -0400

    Adding helpers

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exhbs = require('express-handlebars');
-const hbs = exhbs.create({});
+const helpers = require('./utils/helpers')
+const hbs = exhbs.create({ helpers });
 const path = require('path');
 const dotenv = require('dotenv').config()
 const sequelize = require('./config/connection');
@@ -50,3 +51,44 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+// require('dotenv').config()
+// const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const routes = require('./controllers');
+// const exphbs = require('express-handlebars');
+// const helpers = require('./utils/helpers')
+// const hbs = exphbs.create({ helpers });
+
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+
+// const sess = {
+//   secret: process.env.SECRET,
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: false,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
